70e93a5e5f3a2107edd9770ffc4c561ff337e282
Tomas Sirio <tsirio@bol.com>
2022-07-06 14:02:35 +0200

    Fill the GraphQl configuration. Fill Service methods

---
+++ src/main/java/com/memorable/assetapi/exception/AssetNotFoundException.java
@@ -0,0 +1,5 @@
+package com.memorable.assetapi.exception;
+
+public class AssetNotFoundException extends RuntimeException{
+    public AssetNotFoundException(String message) {super(message);}
+}
--- src/main/java/com/memorable/assetapi/model/ScoreType.java
+++ src/main/java/com/memorable/assetapi/model/ScoreType.java
@@ -1,5 +1,31 @@
 package com.memorable.assetapi.model;
 
+import com.memorable.assetapi.exception.ScoreTypeException;
+
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum ScoreType {
-    SCORE1, SCORE2, SCORE3
+    SCORE1(1),
+    SCORE2(2),
+    SCORE3(3);
+
+    private final int type;
+
+    ScoreType(int type) {
+        this.type = type;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public static ScoreType getScoreType(Integer type) {
+        Optional<ScoreType> scoreTypeOptional = Arrays.stream(ScoreType.values()).filter(
+                scoreType -> scoreType.getType() == type).findFirst();
+        if (scoreTypeOptional.isEmpty()) {
+            throw new ScoreTypeException("Score Type not Supported");
+        }
+        return scoreTypeOptional.get();
+    }
 }
